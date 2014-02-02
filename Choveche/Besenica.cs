17a0eefdcfc6a4485e92f3be8cf6b38b94787f5e
mimipaskova <maria.paskova3@gmail.com>
2014-02-03 01:32:33 +0200

    Besenica version 0.1
    Basic Besenica functionality. With reading from file 50% - done.

---
+++ Choveche/Besenica.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Choveche
+{
+    class Besenica
+    {
+        public static char[] usedLetters = new char[50];
+        public static int k=0;
+        public static int lives = 5;
+        public static char[] finalWord;
+        public static int br=0;
+
+        /// <summary>
+        /// Reading from text file
+        /// </summary>
+        /// <param name="args"></param>
+        static void read()
+        {
+            StreamReader reader = new StreamReader("Words.txt");
+            string[] file = new string[10];
+            string line=reader.ReadLine();
+            while(line  != null)
+            {                
+                Console.WriteLine(line);
+                line = reader.ReadLine();
+            }                   
+            reader.Close();
+        }
+
+        /// <summary>
+        /// Ima li * v dumata
+        /// </summary>
+        /// <returns></returns>
+        static bool isStars(string word)
+        {           
+            return !(br == word.Length);           
+        }
+
+        /// <summary>
+        /// Method
+        /// </summary>
+        /// <param name="myWord">The word to search in</param>
+        /// <param name="myLetter">The letter to search for</param>
+        static void LetterFound(string myWord, char myLetter)
+        {
+            bool isFound=false;
+            if (myLetter == '\r')
+            {
+                return;
+            }
+            for (int i = 0; i <myWord.Length ; i++)
+            {
+                if (myLetter == myWord[i])
+                {
+                    Console.SetCursorPosition(i, 0);
+                    Console.Write(myLetter);
+                    finalWord[i] = myLetter;
+                    isFound = true;
+                    br++;
+                }                
+            }
+
+            if (!isFound)
+            {
+                Console.SetCursorPosition(myWord.Length + 2, 0);
+                Console.WriteLine("{0} jivota",lives-1);
+                lives--;
+                Choveche.DrawChoveche(lives);
+            }
+            
+            Console.SetCursorPosition(0,k+2);
+            usedLetters[k] = myLetter;
+            k++;
+        }
+
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            read();
+            
+            string word = Console.ReadLine();
+            Console.Clear();
+            finalWord = new char[word.Length];
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                Console.Write("*");
+                finalWord[i]='*';
+            }
+            Console.WriteLine();
+
+            
+            
+
+            while (lives > 0 && isStars(word))
+            {
+                //Console.WriteLine(lives);
+                //Console.SetCursorPosition(0, k+1);
+               
+                char letter = (char)Console.Read();
+                
+                LetterFound(word, letter);
+                Console.ReadLine();
+                
+            }            
+
+        }
+
+
+
+
+        public static void mimi()
+        {
+            int[] matrix = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+
+            for (int i = 0; i < Math.Sqrt(matrix.Length); i++)
+            {
+                for (int j = 0; j < Math.Sqrt(matrix.Length); j++)
+                {
+                    Console.Write(matrix[j+i*(int)Math.Sqrt(matrix.Length)]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Choveche/Choveche.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Choveche
+{
+    class Choveche
+    {
+        public  const int BESILO = 4;
+        public  const int HEAD = 3;
+        public  const int ARMS = 2;
+        public  const int BODY = 1;
+        public  const int LEGS = 0;
+
+        public static void DrawChoveche(int lives)
+        {
+           
+            if (lives == BESILO)
+            {
+                Console.SetCursorPosition(Console.BufferWidth / 2, 0);
+                Console.WriteLine("-------------------");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 1);
+                Console.WriteLine("|                  |");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 2);
+                Console.WriteLine("|                  |");
+            }
+            //head
+            if (lives == HEAD)
+            {
+                Console.SetCursorPosition(Console.BufferWidth / 2, 3);
+                Console.WriteLine("|                 ()");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 4);
+                Console.WriteLine("|                ( )");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 5);
+                Console.WriteLine("|               (   )");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 6);
+                Console.WriteLine("|              (     )");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 7);
+                Console.WriteLine("|              (     )");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 8);
+                Console.WriteLine("|               (   )");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 9);
+                Console.WriteLine("|                ( )");
+            }
+            //Arms
+            if (lives == ARMS)
+            {
+                Console.SetCursorPosition(Console.BufferWidth / 2, 10);
+                Console.WriteLine("|           \\     ()    /");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 11);
+                Console.WriteLine("|            \\    |    /");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 12);
+                Console.WriteLine("|             \\   |   /");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 13);
+                Console.WriteLine("|              \\  |  /");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 14);
+                Console.WriteLine("|               \\ | /");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 15);
+                Console.WriteLine("|                \\|/");
+            }
+            //Body
+            if (lives == BODY)
+            {
+                Console.SetCursorPosition(Console.BufferWidth / 2, 16);
+                Console.WriteLine("|                 |");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 17);
+                Console.WriteLine("|                 |");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 18);
+                Console.WriteLine("|                 |");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 19);
+                Console.WriteLine("|                 |");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 20);
+                Console.WriteLine("|                 |");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 21);
+                Console.WriteLine("|                 |");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 22);
+                Console.WriteLine("|                 |");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 23);
+                Console.WriteLine("|                 |");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 24);
+                Console.WriteLine("|                 |");
+            }
+            //Legs
+            if (lives == LEGS)
+            {
+                Console.SetCursorPosition(Console.BufferWidth / 2, 25);
+                Console.WriteLine("|                /\\");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 26);
+                Console.WriteLine("|               /  \\");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 27);
+                Console.WriteLine("|              /    \\");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 28);
+                Console.WriteLine("|             /      \\");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 29);
+                Console.WriteLine("|            /        \\");
+                Console.SetCursorPosition(Console.BufferWidth / 2, 30);
+                Console.WriteLine("|           /          \\");
+            }
+
+        }
+    }
+}
